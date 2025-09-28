f41060bfd9ed67eba458fe661e365f70bee1efee
paws ♡ <135553058+pawslee@users.noreply.github.com>
2025-09-27 20:27:31 -0500

    upload plugin

---
+++ PleaseJustFuckingRestart/Plugin.cs
@@ -0,0 +1,41 @@
+﻿using LabApi.Features.Wrappers;
+using LabApi.Loader.Features.Plugins;
+
+namespace PleaseJustFuckingRestart;
+
+internal class PleaseJustFuckingRestartPlugin : Plugin
+{
+    public override string Name { get; } = "PleaseJustFuckingRestart";
+
+    public override string Description { get; } = "makes soft restart actually fucking restart";
+
+    public override string Author { get; } = "Rue <3";
+
+    public override Version Version { get; } = new(1, 0, 0);
+
+    public override Version RequiredApiVersion { get; } = typeof(Player).Assembly.GetName().Version;
+
+    public override void Enable()
+    {
+        StaticUnityMethods.OnUpdate += CheckForResponses;
+    }
+
+    public override void Disable()
+    {
+        StaticUnityMethods.OnUpdate -= CheckForResponses;
+    }
+
+    private static void CheckForResponses()
+    {
+        if (IdleMode.IdleModeActive)
+        {
+            while (ServerConsole.PrompterQueue.TryDequeue(out string text))
+            {
+                if (!string.IsNullOrWhiteSpace(text))
+                {
+                    ServerConsole.EnterCommand(text, ServerConsole.Scs);
+                }
+            }
+        }
+    }
+}
